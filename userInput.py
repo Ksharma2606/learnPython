7b770dc921ad82e19419ac9f391f0646d15fa03f
Karan Sharma <karansharma06@yahoo.com>
2026-05-21 17:20:20 +0530

    nw comiit

--- userInput.py
+++ userInput.py
@@ -8,12 +8,12 @@ computerchoice = random.choice("123")
 print("Computer chose", computerchoice)
 
 
-if player == int(computerchoice)
+if player == int(computerchoice):
     print("It's a tie")
 
 elif player == 1 and computerchoice == "2":
     print("Computer wins")
-
+    
 elif player == 1 and computerchoice == "3":
     print("Player wins")
 
