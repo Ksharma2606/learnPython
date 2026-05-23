525c4a269e6ac5b8faa4dbf9fd8b672cdf50644c
Ksharma2606 <89061904+Ksharma2606@users.noreply.github.com>
2026-05-23 13:41:41 +0530

    new

--- lists.py
+++ lists.py
@@ -27,3 +27,27 @@ print(users)
 
 users.extend(data)
 print(users)
+
+users.insert(0,'Bob')
+print(users)
+
+users[2:2] = ['Eddie', 'Alex']
+print(users)
+
+users[1:3] = ['Robert', 'JPJ']
+print(users)
+
+users.remove('Bob')
+print(users)
+
+print(users.pop())
+print(users)
+
+del users[0]
+print(users)
+
+data.clear()
+print(data)
+
+users.sort()
+print(users)
